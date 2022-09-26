505b715bff9f0996c8cae8b9366470aea8bcff7e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-09-26 12:17:18 +0300

    added a function to animate counter on the data analysis section

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,30 @@ $(document).ready(function () {
     },
   });
 });
+
+// Sticky Header on Scroll
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Counter
+const counters = document.querySelectorAll(".number");
+const speed = 200;
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.round(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
